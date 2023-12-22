3a0a21120c2ca01eb3245e5dcdd3900fde16f665
HabibSantoso <habib.santoso.ft18@mail.umy.ac.id>
2023-12-22 15:33:59 +0700

    add HomeScreen.kt and KontakApp.kt

---
+++ app/src/main/java/com/example/kontakappwithapi/ui/KontakApp.kt
@@ -0,0 +1,53 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
+package com.example.kontakappwithapi.ui
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.TopAppBarScrollBehavior
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.stringResource
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.kontakappwithapi.R
+import com.example.kontakappwithapi.ui.home.screen.HomeScreen
+import com.example.kontakappwithapi.ui.home.viewmodel.HomeViewModel
+
+@Composable
+fun KontakApp(
+    homeViewModel: HomeViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+    val scrollBehavior = TopAppBarDefaults.exitUntilCollapsedScrollBehavior()
+    Scaffold(
+        modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {TopAppBar(scrollBehavior = scrollBehavior)}
+    ) {
+        Surface(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(it)
+        ) {
+            HomeScreen(kontakUIState = homeViewModel.kontakUIState,
+                retryAction = homeViewModel::getKontak)
+        }
+    }
+}
+
+@Composable
+fun TopAppBar(scrollBehavior: TopAppBarScrollBehavior, modifier: Modifier = Modifier) {
+        CenterAlignedTopAppBar(
+            scrollBehavior = scrollBehavior,
+            title = {
+                Text(text = stringResource(id = R.string.app_name),
+                    style = MaterialTheme.typography.headlineSmall)
+            },
+            modifier = modifier)
+}
