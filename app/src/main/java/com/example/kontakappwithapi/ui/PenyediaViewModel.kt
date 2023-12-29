42839a2511b0db652039cb1ef785f50df77f02b0
HabibSantoso <habib.santoso.ft18@mail.umy.ac.id>
2023-12-29 12:00:25 +0700

    add InsertViewmodel ke dalam PenyediaViewModel.kt

--- app/src/main/java/com/example/kontakappwithapi/ui/PenyediaViewModel.kt
+++ app/src/main/java/com/example/kontakappwithapi/ui/PenyediaViewModel.kt
@@ -6,12 +6,17 @@ import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.kontakappwithapi.KontakAplication
 import com.example.kontakappwithapi.ui.home.viewmodel.HomeViewModel
+import com.example.kontakappwithapi.ui.kontak.viewmodel.InsertViewModel
 
 object PenyediaViewModel {
     val Factory = viewModelFactory {
 
         initializer {
             HomeViewModel(aplikasiMars().container.kontakRepository)
+        }
+
+        initializer {
+            InsertViewModel(aplikasiMars().container.kontakRepository)
         }
     }
 }
