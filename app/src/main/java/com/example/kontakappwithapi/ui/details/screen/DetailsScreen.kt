d5eb0d81a04048d3ca2603e76c3af8647ae9025e
HabibSantoso <habib.santoso.ft18@mail.umy.ac.id>
2023-12-29 16:11:52 +0700

    update DetailsScreen.kt agar bisa menampilkan data

--- app/src/main/java/com/example/kontakappwithapi/ui/details/screen/DetailsScreen.kt
+++ app/src/main/java/com/example/kontakappwithapi/ui/details/screen/DetailsScreen.kt
@@ -1,4 +1,4 @@
-@file:OptIn(ExperimentalMaterial3Api::class)
+@file:OptIn(ExperimentalMaterial3Api::class, ExperimentalMaterial3Api::class)
 
 package com.example.kontakappwithapi.ui.details.screen
 
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
@@ -62,7 +63,11 @@ fun DetailsScreen(
                 )
         }
     ) {innerPadding ->
-
+        DetailStatus(
+            kontakUiState = detailsViewModel.detailsKontakUiState,
+            retryAtion = { detailsViewModel.getKontakById() },
+            onEditClick = onEditClick,
+            modifier = Modifier.padding(innerPadding).fillMaxSize())
     }
 
 }
