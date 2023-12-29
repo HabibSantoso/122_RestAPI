64a42231a34a79b5cf0b5add3c2b560981b59693
HabibSantoso <habib.santoso.ft18@mail.umy.ac.id>
2023-12-29 16:12:47 +0700

    update PengelolahHalaman.kt agar bisa passing(pindah) halaman

--- app/src/main/java/com/example/kontakappwithapi/navigation/PengelolahHalaman.kt
+++ app/src/main/java/com/example/kontakappwithapi/navigation/PengelolahHalaman.kt
@@ -3,9 +3,15 @@ package com.example.kontakappwithapi.navigation
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.kontakappwithapi.ui.details.screen.DetailsDestination
+import com.example.kontakappwithapi.ui.details.screen.DetailsScreen
+import com.example.kontakappwithapi.ui.edit.screen.EditDestination
+import com.example.kontakappwithapi.ui.edit.screen.ItemEditScreen
 import com.example.kontakappwithapi.ui.home.screen.DestinasiHome
 import com.example.kontakappwithapi.ui.home.screen.HomeScreen
 import com.example.kontakappwithapi.ui.kontak.screen.DestinasiEntry
@@ -23,7 +29,9 @@ fun PengelolaHalaman(navController: NavHostController = rememberNavController())
             HomeScreen(navigateToItemEntry = {
                 navController.navigate(DestinasiEntry.route)
             },
-                onDetailClick = {
+                onDetailClick = {itemId ->
+                    navController.navigate("${DetailsDestination.route}/$itemId")
+                    println(itemId)
                 })
         }
         composable(DestinasiEntry.route) {
@@ -36,6 +44,38 @@ fun PengelolaHalaman(navController: NavHostController = rememberNavController())
                     }
                 }
             })
+        }
+        composable(DetailsDestination.routeWithArg,
+            arguments = listOf(navArgument(DetailsDestination.kontakId){
+                type = NavType.IntType
+            })
+        ){
+            backStackEntry ->
+            val itemId = backStackEntry.arguments?.getInt(DetailsDestination.kontakId)
+            itemId?.let {
+                DetailsScreen(
+                    onEditClick = {itemId ->
+                        navController.navigate("${EditDestination.route}/$itemId")
+                        println(itemId)
+                    },
+                    navigateBack = { navController.navigateUp() })
+            }
+        }
+        composable(
+            EditDestination.routeWithArg,
+            arguments = listOf(navArgument(DetailsDestination.kontakId){
+                type = NavType.IntType
+            })
+        ){
+            ItemEditScreen(
+                navigateBack = { navController.popBackStack() },
+                onNavigateUp = {
+                    navController.navigate(DestinasiHome.route){
+                        popUpTo(DestinasiHome.route){
+                            inclusive = true
+                        }
+                    }
+                })
         }
     }
 }
